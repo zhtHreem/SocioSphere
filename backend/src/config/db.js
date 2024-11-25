7ee8296458ded4244a613a91e0a1d7468e8c1ec3
Hareem <zainabhareemtayyab@gmail.com>
2024-11-25 22:58:48 +0500

    configured db connection

---
+++ backend/src/config/db.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config(); // Load environment variables from .env file
+
+const connectDB = async () => {
+  try {
+    // Use environment variable or fallback to default URI
+    const mongoURI =
+      process.env.MONGO_URI ||
+      'mongodb+srv://admin:123@eventify.dkeujvr.mongodb.net/sociosphere?retryWrites=true&w=majority&appName=eventify';
+    
+    await mongoose.connect(mongoURI, {
+    });
+    
+    console.log('MongoDB connected successfully');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error.message);
+    process.exit(1); // Exit process with failure
+  }
+};
+
+export default connectDB;
