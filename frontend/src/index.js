44d0c4f1aa5d5fba3a20ed8357a336fab1b33f13
afeefaKhaliq <afeefakhaliq045@gmail.com>
2024-11-28 00:04:45 +0500

    society catalogue

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,20 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider ,createBrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
-
-
+import Add from './Components/addSociety';
+import Society from './Components/SocietyPage'
 
 const router = createBrowserRouter([
  
   {
     path:"/",
     element:<App/>
+  },{
+    path:"/add",
+    element:<Add/>
+  },{
+    path:"/society",
+    element:<Society/>
   },
  
  
