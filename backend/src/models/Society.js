7db9079fab4dfd52ed02c007dde103c94cd66500
Hareem Tayyab <44234547+zhtHreem@users.noreply.github.com>
2024-11-29 14:12:55 +0500

    Updated

--- backend/src/models/Society.js
+++ backend/src/models/Society.js
@@ -3,6 +3,12 @@ const SocietySchema = new mongoose.Schema({
   description: { type: String, required: true },
   image: { type: String, required: true }, // URL or path to the image
   image: { type: String, required: true },
+  positions: [
+    {
+      title: { type: String, required: true }, // Position name (e.g., "President", "Officer")
+      users: [{type:String}]// type: mongoose.Schema.Types.ObjectId, ref: 'User' }], // Array of users holding the position
+    },
+  ],
   events: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Event' }] // Reference to Event model
 });
 
