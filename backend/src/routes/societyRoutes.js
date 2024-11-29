ef6960f5c35cbb2da3b2c5cad34da61d15f3da53
Hareem Tayyab <44234547+zhtHreem@users.noreply.github.com>
2024-11-29 14:13:51 +0500

    Updated

--- backend/src/routes/societyRoutes.js
+++ backend/src/routes/societyRoutes.js
@@ -27,6 +27,28 @@ router.get('/', async (req, res) => {
   }
 });
 
+// Update positions for a specific society
+router.put('/:id/positions',authMiddleware, async (req, res) => {
+  const { id } = req.params; // Society ID
+  const { positions } = req.body; // Updated positions data
+  console.log("id",id)
+  console.log("po",positions)
+  try {
+    const updatedSociety = await Society.findByIdAndUpdate(
+      id,
+      { positions },
+      { new: true } // Return the updated document
+    );
+
+    if (!updatedSociety) {
+      return res.status(404).json({ error: 'Society not found' });
+    }
+     res.status(200).json(updatedSociety);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to update positions', details: error.message });
+  }
+});
+
 router.get('/:id', async (req, res) => {
   try {
     const society = await Society.findById(req.params.id);
